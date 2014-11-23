af97b5809dca338d57818becc365b3fa023543b0
mesha <ggg4r@yahoo.com>
2014-11-23 23:09:27 +0100

    Create config.php

---
+++ 5/config.php
@@ -0,0 +1,18 @@
+<?php
+
+ob_start();
+
+$host = 'localhost';
+$user = '889478';
+$pass = 'mars1234';
+$test='889478';
+$conn=mysql_connect($host,$user,$pass);
+mysql_select_db($test);
+mysql_query("SET NAMES 'utf8'");
+mysql_query("SET CHARACTER SET utf8"); 
+mysql_query("SET COLLATION_CONNECTION = 'utf8_general_ci'");
+
+
+
+
+?>
